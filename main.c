08d0ecac5e0435f01b4efc020c8288ae88419092
Garnier Tristan <126480368+Demonta0109@users.noreply.github.com>
2023-11-09 13:12:13 +0100

    Ajout de la fonction insert_sorted mais a débuger

--- main.c
+++ main.c
@@ -10,13 +10,11 @@ int main(){
 
     t_d_list *L;
     L = create_list(5);
-    print_aligned_list(L);
     t_d_cell *C;
     C = create_cell(5, 5);
-    insert_head(L, C);
-    print_aligned_list(L);
+    insert_sorted(L, C);
     C = create_cell(2, 3);
-    insert_head(L, C);
-    print_aligned_list(L);
+    insert_sorted(L, C);
+    print_list(L);
     return 0;
 };
--- structure.c
+++ structure.c
@@ -58,20 +58,20 @@ void insert_sorted_at_level(t_d_list *list, t_d_cell* cell, int level){
         return;
     }
     t_d_cell *prev = list->heads[level];
-    t_d_cell *temp = prev->next;
+    t_d_cell *temp = prev->next[level];
     while (temp->next != NULL){
         if (cell->value < temp->value){
-            cell->next = &temp;
-            prev->next = &cell;
+            cell->next[level] = temp;
+            prev->next[level] = cell;
         }else{
-            prev = prev->next;
-            temp = temp->next;
+            prev = prev->next[level];
+            temp = temp->next[level];
             }
         }
-        if (temp->next == NULL){
-            temp->next = &cell;
-        }
+    if (temp->next == NULL){
+        temp->next[level] = cell;
     }
+}
 
 void insert_sorted(t_d_list *list, t_d_cell* cell) {
     for (int i = 0; i < cell->max_levels; i++) {
@@ -96,68 +96,5 @@ void print_level(t_d_list *list, int level) {
 void print_list(t_d_list *list) {
     for (int i = 0; i < list->max_levels; i++) {
         print_level(list, i);
-    }
-}
-
-int n_digit(int n) {
-    return floor(log10(abs(n))) + 1;
-}
-
-int index_of(int val, t_d_list list) {
-    int index = 0;
-    t_d_cell *cell = list.heads[0];
-    while (cell != NULL) {
-        if (cell->value == val) {
-            return index;
-        }
-        index++;
-        cell = cell->next[0];
-    }
-    return -1;
-}
-
-void print_aligned_level(t_d_list *list, int level) {
-    if (level < 0 || level >= list->max_levels) {
-        printf("Error: level %d does not exist\n", level);
-        return;
-    }
-    int max_n = pow(2, list->max_levels)-1;
-    int n_max = n_digit(max_n); // longueur max du chiffre
-    int n_cell = 0;
-    int i_prev = 0;
-    int i_curr = 0;
-    int delta_i = 0;
-    t_d_cell *prev = NULL;
-    t_d_cell *cell = list->heads[level];
-    printf("[list head_%d]", level);
-    while (cell != NULL) {
-        if (prev == NULL) {
-            i_prev = 0;
-        } else {
-            i_prev = index_of(prev->value, *list);
-        }
-        i_curr = index_of(cell->value, *list);
-        delta_i = i_curr - i_prev;
-        for (int i = 0; i < delta_i * 9 + delta_i * n_max; i++) {
-            printf("-");
-        }
-        n_cell = n_digit(cell->value);
-        printf("-->[ ");
-        for (int i = 0; i < n_max - n_cell; i++) {
-            printf(" ");
-        }
-        printf("%d|@-]", cell->value);
-        prev = cell;
-        cell = cell->next[level];
-        printf("%d", delta_i);
-
-    }
-    printf("-->NULL\n");
-}
-
-
-void print_aligned_list(t_d_list *list) {
-    for (int i = 0; i < list->max_levels; i++) {
-        print_aligned_level(list, i);
     }
 }
