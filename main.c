b11086d2a4c956dc7c622416d09ba2498e004f4e
Valentin <valentin.gourjon@efrei.net>
2023-11-09 12:50:13 +0100

    Add main

---
+++ main.c
@@ -0,0 +1,22 @@
+//
+// Created by valen on 09/11/2023.
+//
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "structure.h"
+
+int main(){
+
+    t_d_list *L;
+    L = create_list(5);
+    print_aligned_list(L);
+    t_d_cell *C;
+    C = create_cell(5, 5);
+    insert_head(L, C);
+    print_aligned_list(L);
+    C = create_cell(2, 3);
+    insert_head(L, C);
+    print_aligned_list(L);
+    return 0;
+};
